9f57621a05edad3c1057a651b7c790f5d38eb38a
KatSevernaya <severovakat@gmail.com>
2021-03-04 12:46:21 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $(".reviews__list").slick({
+        infinite: true,
+         slidesToShow: 1,
+         //slidesToScroll: 3,
+         centralMode: true,
+         dots: true,
+         responsive: [{
+
+            breakpoint: 992,
+            settings: {
+                arrows: false 
+            }
+      
+          }]
+
+    })
+
+
+    //Таймер обратного отсчета
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+          'total': t,
+          'days': days,
+          'hours': hours,
+          'minutes': minutes,
+          'seconds': seconds
+        };
+      }
+       
+      function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+       
+        function updateClock() {
+          var t = getTimeRemaining(endtime);
+       
+          daysSpan.innerHTML = t.days;
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+       
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
+        }
+       
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+      }
+       
+      var deadline = new Date(Date.parse(new Date()) + 17* 24 * 60 * 60 * 1000); // for endless timer
+      initializeClock('countdown', deadline);
+
+
+    //Валидация инпута
+
+
+    document.getElementById('phone').addEventListener('keyup', function(){
+        this.value = this.value.replace(/[^\d]/g, '')
+    })
+
+   
+})
+  
